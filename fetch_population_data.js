db45a10c30e4b1fcc6e6378bad3eda5e333d3d28
Joona Niemenmaa <joonaoskari@hotmail.com>
2025-09-22 18:55:32 +0300

    refaktorointia

--- fetch_population_data.js
+++ fetch_population_data.js
@@ -20,41 +20,33 @@ function add_data_row(municipality, population) {
     even = !even;
 }
 
-const url =
+async function fetch_data(url, query) {
+    const data = await (await fetch(query)).json();
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    };
+    var response = await fetch(url, options);
+    return await response.json();
+}
+
+const population_url =
     "https://pxdata.stat.fi/PxWeb/api/v1/fi/StatFin/vaerak/statfin_vaerak_pxt_11ra.px";
+const employment_url =
+    "https://pxdata.stat.fi/PxWeb/api/v1/fi/StatFin/tyokay/statfin_tyokay_pxt_115b.px";
 
-fetch("fetch.json")
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data);
-        const options = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-        };
+fetch_data(population_url, "population_query.json").then((data) => {
+    console.log(data);
 
-        return fetch(url, options);
-    })
-    .then((response) => {
-        console.log("Received response: ${response.status}");
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data);
-
-        var key_to_index = data.dimension.Alue.category.index;
-        var populations = data.value;
-        console.log(key_to_index);
-        console.log(populations);
-
-        for ([key, label] of Object.entries(
-            data.dimension.Alue.category.label,
-        )) {
-            var population = populations[key_to_index[key]];
-            add_data_row(label, population);
-        }
-    });
+    var key_to_index = data.dimension.Alue.category.index;
+    var populations = data.value;
+    console.log(key_to_index);
+    console.log(populations);
+    for ([key, label] of Object.entries(data.dimension.Alue.category.label)) {
+        var population = populations[key_to_index[key]];
+        add_data_row(label, population);
+    }
+});
